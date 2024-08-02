8671dce55061b84d9eef0b9f2e61cb296e5b2852
ayrtonaoki <ayrtonsasouza@gmail.com>
2024-08-02 02:25:26 -0300

    Create frontend with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+     message: [],
+    }
+  }
+componentDidMount() {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => this.setState({ message: data.message }));
+  }
+render () {
+    const { message } = this.state
+    return (
+        <h1>{message}</h1>
+    );
+  }
 }
-
 export default App;
